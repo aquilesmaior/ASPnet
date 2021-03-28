480512b625230f744a007802cf8d8a08b71f9702
Aquiles Maior <aquilesmaior@gmail.com>
2021-03-28 19:32:23 -0300

    Seleção de um Droplist exibindo a tabuada em um listbox

---
+++ Exercicios 1/Exercicios 1/Tabuada.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Tabuada.aspx.cs" Inherits="Exercicios_1.Tabuada" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    
+    <form id="form1" runat="server">
+        <div>
+            <h1>Tabuada</h1>
+        </div>
+        <asp:DropDownList ID="dpNum" runat="server">
+        </asp:DropDownList>
+        <asp:Button ID="btnExecutar" runat="server" OnClick="btnExecutar_Click" Text="Exibir Tabuada" />
+        <br />
+        <asp:ListBox ID="lbxDados" runat="server" OnSelectedIndexChanged="lbxDados_SelectedIndexChanged"></asp:ListBox>
+    </form>
+</body>
+</html>
---
+++ Exercicios 1/Exercicios 1/Tabuada.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Exercicios_1 {
+    public partial class Tabuada : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+
+            // a propriedade IsPostBack serve para indicar que já estive ou não nessa página, então ele executa ou não a condição abaixo.
+            if (!IsPostBack) {
+
+                //  Preenche o droplist com números de 2 à 10
+                for (int i = 2; i < 11; i++) {
+                    dpNum.Items.Add(i.ToString());
+
+                }
+            }
+
+        }
+
+        protected void lbxDados_SelectedIndexChanged(object sender, EventArgs e) {
+
+
+        }
+
+
+        protected void btnExecutar_Click(object sender, EventArgs e) {
+
+            lbxDados.Items.Clear();
+            ListItem li = dpNum.SelectedItem;
+            int num = Convert.ToInt32(li.Value);
+            int total = 0;
+            for (int i = 0; i < 11; i++) {
+                total = i * num;
+                lbxDados.Items.Add(i.ToString() + " x " + num.ToString() + " = " + total.ToString());
+            }
+        }
+    }
+}
