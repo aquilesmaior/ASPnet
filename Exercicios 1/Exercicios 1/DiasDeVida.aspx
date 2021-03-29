f0e549ad990736899371145dddb4d16c9a950676
Aquiles Maior <aquilesmaior@gmail.com>
2021-03-29 17:42:12 -0300

    Dias de Vida, usando componente Calendar

---
+++ Exercicios 1/Exercicios 1/DiasDeVida.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DiasDeVida.aspx.cs" Inherits="Exercicios_1.DiasDeVida" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Table ID="Table1" runat="server" Width="667px">
+            </asp:Table>
+        </div>
+        <asp:Calendar ID="Calendar1" runat="server"></asp:Calendar>
+        <asp:Calendar ID="Calendar2" runat="server"></asp:Calendar>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        <asp:Label ID="lbResultado" runat="server" Text="Dias de vida!!!"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Exercicios 1/Exercicios 1/DiasDeVida.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Exercicios_1 {
+    public partial class DiasDeVida : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+
+            Calendar2.SelectedDate = DateTime.Now;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e) {
+            int diaN = 0, anoN = 0, mesN = 0;
+            int diaA = 0, anoA = 0, mesA = 0;
+            diaN = Calendar1.SelectedDate.Day;
+            mesN = Calendar1.SelectedDate.Month * 30;
+            anoN = Calendar1.SelectedDate.Year * 365;
+
+            diaA = Calendar2.SelectedDate.Day;
+            mesA = Calendar2.SelectedDate.Month * 30;
+            anoA = Calendar2.SelectedDate.Year * 365;
+
+            int Total = (diaA + mesA + anoA) - (diaN + mesN + anoN);
+            lbResultado.Text = "Dias de vida: " + Total.ToString();
+        }
+    }
+}
